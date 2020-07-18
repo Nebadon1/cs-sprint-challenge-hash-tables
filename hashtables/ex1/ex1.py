8d7359bc3896000378bd67911435098556a55295
Carlos Mitchell <kakismitchell@gmail.com>
2020-07-17 21:37:27 -0400

    MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,15 @@
+cache = {}
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    for j in range(len(weights)):
+        a = limit - weights[j]
+        if a in cache:
+            return (max(j, cache[a]), min(j, cache[a]))
 
     return None
